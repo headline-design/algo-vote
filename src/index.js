e55a625319efce0c3a64da4c52406c7735816635
headline-design <aaron@headline.dev>
2022-01-12 17:32:20 -0600

    added window object for customization

--- src/index.js
+++ src/index.js
@@ -3,24 +3,42 @@ import "./styles.css";
 import Pipeline from "@pipeline-ui-2/pipeline";
 
 document.getElementById("vote-root").innerHTML = `
-<h1>Algoo Voot</h1>
+<div id="votediv" class="algo-vote-widget" align="center">
+<h1 id="voteTitle">Algoo Voot</h1>
 <select id="selectWallet">
   <option selected value="myAlgoWallet">MyAlgo</option>
   <option value="AlgoSigner">AlgoSigner</option>
   <option value="WalletConnect">WalletConnect</option>
 </select>
-<button id="connect">Connect</button>
-<p id="address"></p>
-<input id="appId" placeholder="App index" type="number"></input>
-<button id="optin">Opt In</button>
+<button id="connect">Connect</button><br><br>
+
+<label>App</label>
+<input disabled id="appId" placeholder="App index" type="number"></input>
+<button id="optin">Register</button><br><br>
+
+<label>Voting Token</label>
+<input disabled placeholder="asset id" type="number" id="asset"></input>
+<button id="asaOpt">Opt in to Token</button><br><br>
+
+<label>Select Option:</label>
 <select id="selectCandidates">
   <option id="candidatea" value="candidatea">A</option>
   <option id="candidateb" value="candidateb">B</option>
 </select>
-<input placeholder="asset id" type="number" id="asset"></input>
-<button id="asaOpt">Opt in to ASA</button>
 <button id="vote">Vote</button>
-<button id="check">Check</button>`
+<button id="check">Check</button>
+</div>`
+
+//uncomment to test window.voteConfig
+window.voteConfig = {title: "Test Poll", asaIndex: 1234567, appId: 1234567, a: "hello world", b: "goodbye world"}
+
+if (window.voteConfig !== undefined){
+  document.getElementById("candidatea").innerText = window.voteConfig.a
+  document.getElementById("candidateb").innerText = window.voteConfig.b
+  document.getElementById("asset").value = window.voteConfig.asaIndex
+  document.getElementById("appId").value = window.voteConfig.appId
+  document.getElementById("voteTitle").innerText = window.voteConfig.title
+}
 
 const wallet = Pipeline.init()
 
@@ -33,7 +51,7 @@ document.getElementById("connect").onclick = function () {
 }
 
 document.getElementById("optin").onclick = function () {
-  let appId = document.getElementById("appId").value || 123456
+  let appId = document.getElementById("appId").value
   Pipeline.optIn(appId, ["register"]).then(data => alert("Transaction status: " + data))
 }
 
@@ -71,6 +89,19 @@ function checkVote() {
           }
         }
       }
-      alert("Voting tallies: Option 1: " + atally + ", Option 2: " + btally)
+      window.tallies = {a: atally, b: btally}
+      //alert("Voting tallies: Option 1: " + atally + ", Option 2: " + btally)
     })
+}
+
+setInterval(toggleBorder,100)
+
+var on = false
+var colora = "8px solid green"
+var colorb = "20px solid red"
+
+function toggleBorder(){
+  on = !on
+  let color = on?colora:colorb
+  document.getElementById("votediv").style.border = color
 }
